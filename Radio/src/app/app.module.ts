9510010c1ee01606ca1afa4c3a12e5c0a3038294
tholakelebusi <tholakelebusi@gmail.com>
2022-02-05 03:22:05 -1100

    service

--- Radio/src/app/app.module.ts
+++ Radio/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -6,11 +7,17 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
---
+++ Radio/src/app/services/stations.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StationsService {
+
+  url: string ="http://localhost:3000/stations";
+ 
+  constructor(private http: HttpClient) {}
+
+  getAllStation() {
+  
+    return this.http.get(this.url).pipe(
+      map(response => response)
+  );
+}
+
+getOne(id:any) {
+  return this.http.get(`${this.url}/${id}`);
+  }
+
+
+ move(id:Number) {
+    return this.http.get(`${this.url}/next/${id}`);
+    }
+
+updateStatus(id:any,body:any)
+{
+  return this.http.put(`${this.url}/next/${id}`,body);
+}
+
+
+searchname(name:any)
+  {
+    return this.http.get(`${this.url}/${name}`);
+  }
+
+
+}
