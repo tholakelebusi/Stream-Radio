76ff96dd5826a36ee8cc0c3887bff3ad66eb7475
tholakelebusi <tholakelebusi@gmail.com>
2022-02-15 00:39:01 -1100

    favou

--- Radio/src/app/tab2/tab2.page.ts
+++ Radio/src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { StationsService } from '../services/stations.service';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,17 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  favourite:any;
+  constructor(private radioService:StationsService) {}
+
+  ngOnInit(): void {
+    let id=localStorage.getItem("favourite");
+    
+    this.radioService.getOne(id).subscribe((stations)=>
+    this.favourite=stations
+    );
+  }
+ 
+
 
 }
